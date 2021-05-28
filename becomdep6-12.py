9ad4a11c07a6d8338fe38a4ea8a27158a9fb6561
Yarramsetty siva satyanarayana <64882708+siva6160@users.noreply.github.com>
2021-05-28 16:48:16 +0530

    Add files via upload

---
+++ becomdep6-12.py
@@ -0,0 +1,11 @@
+num=int(input())
+for i in range(1,num+1):
+    if i%2:
+        for j in range(1,num+1):
+            print(j+1,end="")
+    else:
+        for i in range(1,num+1):
+            print(j+1,end="")
+    print()
+       
+    
